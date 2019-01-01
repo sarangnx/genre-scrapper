af7472df8e6299be925229a8d00ab490ca337b89
Sarang <sarang98divakaran@gmail.com>
2019-01-01 22:56:47 +0530

    Create Lists and extract metadata
    List the files in input_directory
    and extract their metadata (artist,
    title).

--- classifier.js
+++ classifier.js
@@ -1,21 +1,75 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const mm = require('musicmetadata');
+const fs = require('fs');
+const path = require('path');
 
-let string = 'eminem beautiful genre';
-let qstring = encodeURIComponent(string);
+let input_directory  = path.resolve('..','songs','input');
+let output_directory = path.resolve('..','songs','output');
 
-puppeteer.launch().then(async (browser) => {
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com/search?q='+qstring);
-    // console.log(await page.content());
-    let con = await page.$eval('.Z0LcW',el => el.innerHTML );
-    console.log(con);
-    await browser.close();
-}).catch((err) => {
-    console.log("GOD DAMN..!!");
+/**
+ * @var metadata contains array of objects
+ * [ { title, artist, path, filename } ]
+ */
+let metadata = [];
+
+/**
+ * List songs in input_directory
+ * -- Input directory must contain files only.
+ *    NO DIRECTORIES
+ */
+
+fs.readdir(input_directory,(err, files) => {
+    for(let file of files){
+        let song = path.join(input_directory,file); 
+
+        // for extracting metadata using mm
+        let readable = fs.createReadStream(song);
+        /**
+         * extract title, artist from song file,
+         * add it to metadata array.
+         */
+        mm(readable, (err, data) => {
+            let element = {
+                title: data.title,
+                artist: data.artist.toString(),
+                path: song,
+                filename: file
+            }
+            metadata.push(element);
+        });
+    }
 });
 
+// let string = 'eminem beautiful genre';
+// let qstring = encodeURIComponent(string);
 
-// Z0LcW
+// puppeteer.launch().then(async (browser) => {
+//     const page = await browser.newPage();
+//     await page.goto('https://www.google.com/search?q='+qstring);
+//     // console.log(await page.content());
+//     let con = await page.$eval('.Z0LcW',el => el.innerHTML );
+//     console.log(con);
+//     await browser.close();
+// }).catch((err) => {
+//     console.log("GOD DAMN..!!");
+// });
+
+
 // page.$(selector)
 // page.content()
 // page.goto(url[, options])
+
+
+
+
+/**
+ * [ single genre ]
+ * - Z0LcW
+ * [ multiple genres ]
+ *  
+ * - rl_container
+ *    - TZNJBf
+ *        - IAznY
+ *            - title
+ */
+
